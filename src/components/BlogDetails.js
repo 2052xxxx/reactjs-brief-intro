4ae8114e90d313cfaf1109cfeef0e07838e66f30
N Minh Trang <20522039@gm.uit.edu.vn>
2025-12-06 16:16:39 +0700

    NotFound page completed - Course complete

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,12 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
-    const url = 'http://localhost:8000/blogs/' + id;
-    const { data: blog, isPending, error } = useFetch(url);
+    const url = 'http://localhost:8000/blogs/';
+    const { data: blog, isPending, error } = useFetch(url + id);
+    const navigate = useNavigate();
 
+    const handleClick = () => {
+        fetch(url + blog.id, {
+            method: 'DELETE',
+        })
+        .then(() => {
+            navigate("/");
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -17,6 +26,7 @@ const BlogDetails = () => {
                         <h2>{blog.title}</h2>
                         <p className='sub'>Written by {blog.author}</p>
                         <div>{blog.body}</div>
+                        <button onClick={handleClick}>delete</button>
                     </article>
                 ))
             }
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Will Graham");
+    const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        // setTimeout(() => {
+        // }, 1000)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        })
+        .then(() => {
+            console.log('form submitted!', blog)
+            setIsPending(false);
+            // navigate(-1);
+            navigate("/");
+        })
+
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Will Graham">Will Graham</option>
+                    <option value="Hannibal Lecter">Hannibal Lecter</option>
+                    <option value="fathers' daughter">fathers' daughter</option>
+                </select>
+                { isPending ? <button disabled >Uploading...</button>: <button>Add Blog</button>}
+
+                {/* <p>{author}</p> */}
+
+            </form>
         </div>
     );
 }
